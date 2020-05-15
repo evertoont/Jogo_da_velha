2f69af98ad5e089e10564dfa2a1f35b42b83f0b8
evertoont <evertontbr@gmail.com>
2020-05-15 19:18:36 -0300

    Adiciona função de apresentação do tabuleiro

---
+++ Jogo_da_velha.py
@@ -0,0 +1,13 @@
+def printar_tabuleiro(tabuleiro):
+
+    print('     |     |       ')
+    print(f'  {tabuleiro[1]}  |  {tabuleiro[2]}  |  {tabuleiro[3]}')
+    print('     |     |       ')
+    print('------------------')
+    print('     |     |       ')
+    print(f'  {tabuleiro[4]}  |  {tabuleiro[5]}  |  {tabuleiro[6]}')
+    print('     |     |       ')
+    print('------------------')
+    print('     |     |       ')
+    print(f'  {tabuleiro[7]}  |  {tabuleiro[8]}  |  {tabuleiro[9]}')
+    print('     |     |       ')
